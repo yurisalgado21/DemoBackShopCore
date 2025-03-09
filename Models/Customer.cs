c66a0efbe517bb361730803e64cd935c680a8aec
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-03-08 21:23:15 -0300

    feat: Customer Properties Added

---
+++ Models/Customer.cs
@@ -0,0 +1,11 @@
+namespace DemoBackShopCore.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
